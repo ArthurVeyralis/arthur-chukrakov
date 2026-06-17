b60c3f08b21cc51739791e8179a51287e6376a71
Arthur Chukrakov <arthur.veyralis@gmail.com>
2026-06-17 15:44:35 +0700

    Remove Node version restriction, downgrade pnpm to v9

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
+/// <reference types="vite/client" />
+
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit(),
-	],
+	plugins: [tailwindcss(), sveltekit()],
 });
